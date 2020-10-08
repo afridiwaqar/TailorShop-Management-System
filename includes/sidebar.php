daf50600e7ba725f0d6babd7b63e1464692bbc09
MusheAbdul-Hakim <musheabdulhakiim99@gmail.com>
2020-10-08 20:59:25 +0000

    maintaining clean code

---
+++ includes/sidebar.php
@@ -0,0 +1,42 @@
+<div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
+      <div class="main-menu-content">
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+          <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+            <ul class="menu-content">
+              <li><a class="menu-item" href="dashboard-ecommerce.html"><i></i><span data-i18n="eCommerce">eCommerce</span></a>
+              </li>
+              <li class="active"><a class="menu-item" href="dashboard-crypto.html"><i></i><span data-i18n="Crypto">Crypto</span></a>
+              </li>
+              <li><a class="menu-item" href="dashboard-sales.html"><i></i><span data-i18n="Sales">Sales</span></a>
+              </li>
+            </ul>
+          </li>
+          <li class=" nav-item"><a href="#"><i class="la la-television"></i><span class="menu-title" data-i18n="Templates">Templates</span></a>
+            <ul class="menu-content">
+              <li><a class="menu-item" href="#"><i></i><span data-i18n="Vertical">Vertical</span></a>
+                <ul class="menu-content">
+                  <li><a class="menu-item" href="vertical-menu-template"><i></i><span data-i18n="Classic Menu">Classic Menu</span></a>
+                  </li>
+                  <li><a class="menu-item" href="vertical-modern-menu-template"><i></i><span data-i18n="Modern Menu">Modern Menu</span></a>
+                  </li>
+                  <li><a class="menu-item" href="vertical-collapsed-menu-template"><i></i><span data-i18n="Collapsed Menu">Collapsed Menu</span></a>
+                  </li>
+                  <li><a class="menu-item" href="vertical-compact-menu-template"><i></i><span data-i18n="Compact Menu">Compact Menu</span></a>
+                  </li>
+                  <li><a class="menu-item" href="vertical-content-menu-template"><i></i><span data-i18n="Content Menu">Content Menu</span></a>
+                  </li>
+                  <li><a class="menu-item" href="vertical-overlay-menu-template"><i></i><span data-i18n="Overlay Menu">Overlay Menu</span></a>
+                  </li>
+                </ul>
+              </li>
+             </ul>
+           </li>
+          <li class=" navigation-header"><span>Support</span><i class="la la-ellipsis-h" data-toggle="tooltip" data-placement="right" data-original-title="Support"></i>
+          </li>
+          <li class=" nav-item"><a href="https://pixinvent.ticksy.com/" target="_blank"><i class="la la-support"></i><span class="menu-title" data-i18n="Raise Support">Raise Support</span></a>
+          </li>
+          <li class=" nav-item"><a href="https://pixinvent.com/modern-admin-clean-bootstrap-4-dashboard-html-template/documentation" target="_blank"><i class="la la-text-height"></i><span class="menu-title" data-i18n="Documentation">Documentation</span></a>
+          </li>
+        </ul>
+      </div>
+    </div>
