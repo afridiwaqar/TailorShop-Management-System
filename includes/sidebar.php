ae81ab6f81dcfe1fd743767e7e4f533c9fed659d
MusheAbdul-Hakim <musheabdulhakiim99@gmail.com>
2020-10-08 22:25:08 +0000

    sidebar properties added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,41 +1,41 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
       <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-          <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+          <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span></a>
             <ul class="menu-content">
-              <li><a class="menu-item" href="dashboard-ecommerce.html"><i></i><span data-i18n="eCommerce">eCommerce</span></a>
-              </li>
-              <li class="active"><a class="menu-item" href="dashboard-crypto.html"><i></i><span data-i18n="Crypto">Crypto</span></a>
-              </li>
-              <li><a class="menu-item" href="dashboard-sales.html"><i></i><span data-i18n="Sales">Sales</span></a>
+              <li><a class="menu-item" href="index.php"><i></i>
+              <span data-i18n="eCommerce">Dashboard</span></a>
               </li>
             </ul>
           </li>
-          <li class=" nav-item"><a href="#"><i class="la la-television"></i><span class="menu-title" data-i18n="Templates">Templates</span></a>
+          <li class=" nav-item"><a href="#"><i class="la la-television"></i>
+          <span class="menu-title" data-i18n="Templates">Projects</span></a>
             <ul class="menu-content">
-              <li><a class="menu-item" href="#"><i></i><span data-i18n="Vertical">Vertical</span></a>
-                <ul class="menu-content">
-                  <li><a class="menu-item" href="vertical-menu-template"><i></i><span data-i18n="Classic Menu">Classic Menu</span></a>
-                  </li>
-                  <li><a class="menu-item" href="vertical-modern-menu-template"><i></i><span data-i18n="Modern Menu">Modern Menu</span></a>
-                  </li>
-                  <li><a class="menu-item" href="vertical-collapsed-menu-template"><i></i><span data-i18n="Collapsed Menu">Collapsed Menu</span></a>
-                  </li>
-                  <li><a class="menu-item" href="vertical-compact-menu-template"><i></i><span data-i18n="Compact Menu">Compact Menu</span></a>
-                  </li>
-                  <li><a class="menu-item" href="vertical-content-menu-template"><i></i><span data-i18n="Content Menu">Content Menu</span></a>
-                  </li>
-                  <li><a class="menu-item" href="vertical-overlay-menu-template"><i></i><span data-i18n="Overlay Menu">Overlay Menu</span></a>
-                  </li>
-                </ul>
+              <li><a class="menu-item" href="#"><i></i>
+              <span data-i18n="Vertical">Add Project</span></a>
+              </li>
+              <li><a class="menu-item" href="#"><i></i>
+              <span data-i18n="Vertical">All Projects</span></a>
               </li>
              </ul>
            </li>
-          <li class=" navigation-header"><span>Support</span><i class="la la-ellipsis-h" data-toggle="tooltip" data-placement="right" data-original-title="Support"></i>
+           <li class=" nav-item"><a href="#"><i class="la la-bars"></i>
+          <span class="menu-title" data-i18n="Templates">Rocks</span></a>
+            <ul class="menu-content">
+              <li><a class="menu-item" href="#"><i></i>
+              <span data-i18n="Vertical">Add Rock</span></a>
+              </li>
+              <li><a class="menu-item" href="#"><i></i>
+              <span data-i18n="Vertical">All Rocks</span></a>
+              </li>
+             </ul>
+           </li>
+         
+          <li class=" nav-item">
+            <a href="" target="_blank"><i class="la la-support"></i><span class="menu-title" data-i18n="Raise Support">Raise Support</span></a>
           </li>
-          <li class=" nav-item"><a href="https://pixinvent.ticksy.com/" target="_blank"><i class="la la-support"></i><span class="menu-title" data-i18n="Raise Support">Raise Support</span></a>
-          </li>
-          <li class=" nav-item"><a href="https://pixinvent.com/modern-admin-clean-bootstrap-4-dashboard-html-template/documentation" target="_blank"><i class="la la-text-height"></i><span class="menu-title" data-i18n="Documentation">Documentation</span></a>
+          <li class="nav-item"><a href="logout.php" target="_blank"><i class="ft-power"></i>
+          <span class="menu-title" data-i18n="Documentation">Logout</span></a>
           </li>
         </ul>
       </div>
