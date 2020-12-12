5779a2ada7bf3c64407218ea0969fcd385d5b716
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-12 23:19:59 +0000

    updated

--- includes/footer.php
+++ includes/footer.php
@@ -9,5 +9,5 @@
                               );
                           </script></span
                         >
-                        <a class="text-bold-800 grey darken-2" href="https://1.envato.market/modern_admin" target="_blank"> PIXINVENT</a></span><span class="float-md-right d-none d-lg-block">Hand-crafted & Made with<i class="ft-heart pink"></i><span id="scroll-top"></span></span></p>
+                        <a class="text-bold-800 grey darken-2" href="#" target="_blank"> tailorshop</a></span><span class="float-md-right d-none d-lg-block">Hand-crafted & Made with<i class="ft-heart pink"></i><span id="scroll-top"></span></span></p>
 		</footer>
