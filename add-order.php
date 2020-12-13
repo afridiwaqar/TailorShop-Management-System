dfca63b41f1b68ef94b3f7941b0311151c5ac3d9
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-13 02:30:00 +0000

    order form added

--- add-order.php
+++ add-order.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
    
-    <title>Project Summary</title>
+    <title>Add Order</title>
     <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
