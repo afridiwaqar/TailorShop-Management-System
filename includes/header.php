8f6fb6ece8ff7165caa2bb80503c7c111216e835
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-13 00:41:31 +0000

    header add

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
           <ul class="nav navbar-nav flex-row">
             <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
             <li class="nav-item"><a class="navbar-brand" href="index.php"><img class="brand-logo" alt="admin logo" src="app-assets/images/logo/logo.png">
-                <h3 class="brand-text">BFOSTER</h3></a></li>
+                <h3 class="brand-text">TailorShop</h3></a></li>
             <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="la la-ellipsis-v"></i></a></li>
           </ul>
         </div>
@@ -73,7 +73,7 @@
               </li>
               
               <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown"><span class="mr-1 user-name text-bold-700">John Doe</span><span class="avatar avatar-online"><img src="app-assets/images/portrait/small/avatar-s-19.png" alt="avatar"><i></i></span></a>
-                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="user-profile.php"><i class="ft-user"></i> Edit Profile</a>
+                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="change-password.php"><i class="ft-user"></i> Edit Profile</a>
                   <div class="dropdown-divider"></div><a class="dropdown-item" href="login-with-bg-image.php"><i class="ft-power"></i> Logout</a>
                 </div>
               </li>
