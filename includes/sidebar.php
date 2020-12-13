aa666edea3e23fba4c2a1478ba2bf5ee6d540a98
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-13 00:39:39 +0000

    side bar elements added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,35 +1,152 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
       <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-          <li class=" nav-item"><a href="index.php"><i class="la la-dashboard"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="index.php"><i></i>
-              <span data-i18n="eCommerce">Dashboard</span></a>
-              </li>
-            </ul>
+
+          <li class="nav-item">
+            <a href="index.php">
+              <i class="la la-dashboard"></i>
+               <span class="menu-title" data-i18n="Dashboard">Dashboard</span>
+             </a>
           </li>
-        
-          <li class=" nav-item"><a href="#"><i class="icon-pie-chart warning"></i>
-          <span class="menu-title" data-i18n="Templates">Rocks</span></a>
+          <li class="nav-item">
+            <a href="calendar.php">
+              <i class="la la-calendar"></i>
+               <span class="menu-title" data-i18n="Calendar">Calendar</span>
+             </a>
+          </li>
+
+          <li class=" nav-item"><a href="#"><i class="icon-basket-loaded success"></i>
+          <span class="menu-title" data-i18n="Order">Order</span></a>
             <ul class="menu-content">
               <li><a class="menu-item" href="#"><i></i>
-              <span data-i18n="Vertical">Add Rock</span></a>
+              <span data-i18n="Vertical">Add Order</span></a>
               </li>
               <li><a class="menu-item" href="#"><i></i>
-              <span data-i18n="Vertical">All Rocks</span></a>
+              <span data-i18n="Vertical">View/Edit Orders</span></a>
+              </li>
+             </ul>
+           </li>
+        
+          <li class=" nav-item"><a href="#"><i class="icon-users"></i>
+          <span class="menu-title" data-i18n="customers">Customers</span></a>
+            <ul class="menu-content">
+              <li><a class="menu-item" href="#"><i></i>
+              <span data-i18n="Vertical">Add Customer</span></a>
+              </li>
+              <li><a class="menu-item" href="#"><i></i>
+              <span data-i18n="Vertical">View/Edit Customers</span></a>
               </li>
              </ul>
            </li>
 
-           <li class=" nav-item"><a href="#"><i class="la la-briefcase"></i><span class="menu-title" data-i18n="Project">Project</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="project-summary.php"><i></i><span data-i18n="Project Summary">Add Project</span></a>
+           <li class=" nav-item"><a href="#"><i class="la la-group"></i><span class="menu-title" data-i18n="staff management">Staff Management</span></a>
+                <ul class="menu-content">
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="staff">Staff</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="add staff">Add Staff</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="all staff">View/Edit Staff</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a class="menu-item" href="#"><i></i><span data-i18n="salary">Pay Salary</span></a>
+                  </li>
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="designation">Designation</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="add designation">Add Designation</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="View Designation">View/Edit Designation</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </li>
-              <li><a class="menu-item" href="project-tasks.php"><i></i><span data-i18n="Project Task">All Projects</span></a>
+
+             <li class=" nav-item"><a href="#"><i class="la la-briefcase"></i><span class="menu-title" data-i18n="expense management">Expense Management</span></a>
+                <ul class="menu-content">
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="expense">Expense</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="add expense">Add Expense</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="all expense">View/Edit Expense</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                 
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="category">Expense Category</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="add expense category">Add Expense Category</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="View Category">View/Edit Expense Category</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </li>
-              
-            </ul>
-          </li>         
+
+            <li class=" nav-item">
+              <a href="#"><i class="la la-money"></i><span class="menu-title" data-i18n="income management">Income Management</span></a>
+                <ul class="menu-content">
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="income">Income</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="add income">Add Income</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="all income">View/Edit Income</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                 
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="category">Income Category</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="add income category">Add Income Category</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="View income Category">View/Edit Income Category</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+            </li>   
+
+            <li class=" nav-item">
+              <a href="#"><i class="la la-user"></i><span class="menu-title" data-i18n="measurement">Measurement Settings</span></a>
+                <ul class="menu-content">
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="cloth">Cloth</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="cloth type">Add Cloth Type</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="clothes">View/Edit Clothes</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                 
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="parts">Measurement Parts</span></a>
+                    <ul class="menu-content">
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="set parts">Set Measurement Parts</span></a>
+                      </li>
+                      <li><a class="menu-item" href="#"><i></i><span data-i18n="view measurement parts">View/Edit Measurement Parts</span></a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+            </li>  
+
+           <li class=" nav-item">
+              <a href="#"><i class="la la-gear"></i><span class="menu-title" data-i18n="general settings">General Settings</span></a>
+                <ul class="menu-content">
+                  <li>
+                    <a class="menu-item" href="#"><i></i><span data-i18n="settings">Settings</span></a>
+                    
+                  </li>                  
+                </ul>
+            </li>   
           
           <li class="nav-item"><a href="logout.php" target="_blank"><i class="ft-power danger"></i>
           <span class="menu-title" data-i18n="Documentation">Logout</span></a>
