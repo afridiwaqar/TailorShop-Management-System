80e68c5d8853c58bce2b03a985aa1357bed0ed38
MusheAbdulHakim <musheabdulhakim99@gmail.com>
2020-12-13 00:41:00 +0000

    dashboard updated

--- index.php
+++ index.php
@@ -61,46 +61,7 @@
 				</div>
 				<div class="content-body"><!-- eCommerce statistic -->
 <div class="row">
-		<div class="col-xl-3 col-lg-6 col-12">
-				<div class="card pull-up">
-						<div class="card-content">
-								<div class="card-body">
-										<div class="media d-flex">
-												<div class="media-body text-left">
-														<h3 class="info">850</h3>
-														<h6>Products Sold</h6>
-												</div>
-												<div>
-														<i class="icon-basket-loaded info font-large-2 float-right"></i>
-												</div>
-										</div>
-										<div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-												<div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-										</div>
-								</div>
-						</div>
-				</div>
-		</div>
-		<div class="col-xl-3 col-lg-6 col-12">
-				<div class="card pull-up">
-						<div class="card-content">
-								<div class="card-body">
-										<div class="media d-flex">
-												<div class="media-body text-left">
-														<h3 class="warning">$748</h3>
-														<h6>Net Profit</h6>
-												</div>
-												<div>
-														<i class="icon-pie-chart warning font-large-2 float-right"></i>
-												</div>
-										</div>
-										<div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-												<div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-										</div>
-								</div>
-						</div>
-				</div>
-		</div>
+		
 		<div class="col-xl-3 col-lg-6 col-12">
 				<div class="card pull-up">
 						<div class="card-content">
@@ -108,10 +69,10 @@
 										<div class="media d-flex">
 												<div class="media-body text-left">
 														<h3 class="success">146</h3>
-														<h6>New Customers</h6>
+														<h6>Total Customers!</h6>
 												</div>
 												<div>
-														<i class="icon-user-follow success font-large-2 float-right"></i>
+														<i class="icon-users success font-large-2 float-right"></i>
 												</div>
 										</div>
 										<div class="progress progress-sm mt-1 mb-0 box-shadow-2">
@@ -127,11 +88,52 @@
 								<div class="card-body">
 										<div class="media d-flex">
 												<div class="media-body text-left">
-														<h3 class="danger">99.89 %</h3>
-														<h6>Customer Satisfaction</h6>
+														<h3 class="info">850</h3>
+														<h6>Total Orders!</h6>
 												</div>
 												<div>
-														<i class="icon-heart danger font-large-2 float-right"></i>
+														<i class="icon-basket-loaded info font-large-2 float-right"></i>
+												</div>
+										</div>
+										<div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+												<div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+
+								</div>
+						</div>
+				</div>
+		</div>
+		<div class="col-xl-3 col-lg-6 col-12">
+				<div class="card pull-up">
+						<div class="card-content">
+								<div class="card-body">
+										<div class="media d-flex">
+												<div class="media-body text-left">
+														<h3 class="warning">$748</h3>
+														<h6>Last 30 Days Income!</h6>
+												</div>
+												<div>
+														<i class="icon-money warning font-large-2 float-right"></i>
+												</div>
+										</div>
+										<div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+												<div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+								</div>
+						</div>
+				</div>
+		</div>
+		<div class="col-xl-3 col-lg-6 col-12">
+				<div class="card pull-up">
+						<div class="card-content">
+								<div class="card-body">
+										<div class="media d-flex">
+												<div class="media-body text-left">
+														<h3 class="danger">99.89 %</h3>
+														<h6>Last 30 Days Expenses!</h6>
+												</div>
+												<div>
+														<i class="icon-credit-card  danger font-large-2 float-right"></i>
 												</div>
 										</div>
 										<div class="progress progress-sm mt-1 mb-0 box-shadow-2">
